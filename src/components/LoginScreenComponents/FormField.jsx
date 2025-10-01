bae5017beb04c4c4df8f3143b474472e7795e081
Thiago_de_Almeida_Brum <thiagobrum222@gmail.com>
2025-10-01 13:14:45 -0300

    Setimo commit

--- src/components/LoginScreenComponents/FormField.jsx
+++ src/components/LoginScreenComponents/FormField.jsx
@@ -1,11 +1,29 @@
 import MessageAfter from "./MessageAfter";
 
-function FormField({value, theme}){
+function FormField({value, theme, functionTheme}){
     return (
         <div id="form-field" className="flex flex-col w-[100%] sm:mb-6 mb-3">
-            <label className={`mb-2 ${theme ? "text-slate-950" : "text-slate-500"}`} htmlFor={`${value.type}`}>{value.name}</label>
+            <label 
+                className={
+                    `mb-2 
+                    ${theme ? "text-slate-950" : "text-slate-500"}`}
+                    htmlFor={`${value.type}`
+                }>
+                {value.name}
+            </label>
 
-            <input className={`py-2 px-4 ${theme ? "bg-slate-200  placeholder:text-slate-500" : "bg-slate-900 border-[1px] border-slate-500 placeholder:text-slate-400"} rounded-[6px]  font-[500px] outline-none ${value.name === "Senha" ? "mb-2" : null}`} type={`${value.type}`} placeholder={value.placeholder} name={`${value.type}`} id={`${value.type}`}/>
+            <input 
+                className={
+                    `py-2 px-4 
+                    ${functionTheme(theme,"bg-slate-200  placeholder:text-slate-500 text-slate-950","bg-slate-900 border-[1px] border-slate-500 placeholder:text-slate-400 text-slate-300")}
+                    rounded-[6px]  font-[500px] outline-none 
+                    ${value.name === "Senha" ? "mb-2" : null}`
+                } 
+                type={`${value.type}`} 
+                placeholder={value.placeholder} 
+                name={`${value.type}`} 
+                id={`${value.type}`}
+                />
 
             {value.name === "Senha" ? <MessageAfter message1={"Esqueceu sua senha?"} message2={"Clique Aqui"} size={"text-[13px]"}/> : null}
         </div>
--- src/pages/LoginScreen.jsx
+++ src/pages/LoginScreen.jsx
@@ -17,11 +17,14 @@ function LoginScreen(){
         setTheme((prev) => !prev)
     }
 
+    function validateTheme(theme, light, dark){
+        return theme ? light : dark;
+    }
 
 
     return (
         <div id="login-screen" className="w-full min-h-dvh flex md:flex-row flex-col">
-            <section id="login-screen-section-one" className={` hidden sm:block w-full min-h-full bg-gradient-to-tr ${theme ? "from-indigo-400 to-indigo-300" : " from-slate-950 to-slate-800"} py-6`}>
+            <section id="login-screen-section-one" className={` hidden sm:block w-full min-h-full bg-gradient-to-tr ${validateTheme(theme, "from-indigo-400 to-indigo-300", "from-slate-950 to-slate-800")} py-6`}>
                 <div className="h-[50%] w-full flex justify-center items-center">
                     <div id="box" className="space-y-6">
                         <h1 className="text-slate-50 text-4xl text-left font-[700]
@@ -33,10 +36,10 @@ function LoginScreen(){
                     <img id="logo" className="max-h-[180px] max-w-[242px]" src="/Vector.png" alt="" />
                 </div>
             </section>
-            <section id="login-screen-div-two" className={`${theme ? "bg-slate-50" : "bg-slate-900"} w-full min-h-dvh flex items-center justify-between py-8 flex-col`}>
+            <section id="login-screen-div-two" className={`${validateTheme(theme, "bg-slate-50", "bg-slate-900")} w-full min-h-dvh flex items-center justify-between py-8 flex-col`}>
                 <div className="max-w-[436px] w-full flex flex-col items-center md:mt-[100px]">
                     <div className="flex justify-center flex-col w-full">
-                        <div className={`${theme ? "text-slate-950" : "text-slate-500"} w-[100%] px-6`}>
+                        <div className={`${validateTheme(theme, "text-slate-950", "text-slate-500")} w-[100%] px-6`}>
                             <h1 className="font-[700] text-[28px]">Faça seu login agora!</h1>
                             <p className="text-[16px] mb-0 md:mb-[70px]">Deixe seus exames em dia.</p>
                         </div>
@@ -44,7 +47,7 @@ function LoginScreen(){
 
                             {
                                 fields.map((field, index) => {
-                                    return <FormField key={index} value={field} theme={theme}/>
+                                    return <FormField key={index} value={field} theme={theme} functionTheme={validateTheme}/>
                                 })
                             }
                             <ButtonMain 
@@ -62,14 +65,14 @@ function LoginScreen(){
                         padding={"px-6"}
                     />
                 </div>
-                <div id="container-theme" className="flex flex-col gap-4 items-center mt-[28px] sm:gap-12 sm:flex-row">
+                <div id="container-theme" className="flex flex-col gap-4 items-center sm:gap-12 sm:flex-row">
                     <div className="flex gap-1 items-center">
 
                         <Lightbulb 
                             className={`m-0 p-0 text-slate-500 font-[500] `}
                         />
                         
-                        <p className="m-0 p-0 text-slate-500 font-[500]">{theme ? "Modo escuro" : "Modo claro"}</p>
+                        <p className="m-0 p-0 text-slate-500 font-[500]">{validateTheme(theme, "Modo escuro", "Modo claro")}</p>
 
                     </div>
                     <div className="flex items-center">
