7d21c3059cd4de3cab2648c7c3b91e23bae5127c
Thiago_de_Almeida_Brum <thiagobrum222@gmail.com>
2025-10-01 22:22:25 -0300

    Decimo commit

--- src/components/LoginComponents/MessageAfterLink.jsx
+++ src/components/LoginComponents/MessageAfterLink.jsx
@@ -1,8 +1,13 @@
+import { Link } from "react-router-dom"
+
 function MessageAfterLink({message1, message2, link, size, flexAlign, flexJustify}){
     return (
         <div className={`flex ${flexAlign} ${flexJustify} my-[10px]`}>
             <p className={`m-0 p-0 text-slate-500 font-[600] ${size}`}>
-                {message1} <a href={`${link}`} className={`text-indigo-700 font-[600] ${size}`}>{message2}</a>
+                {message1} <Link to={link} className={`text-indigo-700 font-[600] ${size}`}>
+                    {message2}
+                </Link>
+                
             </p>
         </div>
     )
