cf10483d86ddea63d1fe4257ca1f24bb85b43606
Thiago_de_Almeida_Brum <thiagobrum222@gmail.com>
2025-10-04 20:15:12 -0300

    Decimo Quinto commit

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,6 +1,22 @@
+import { useNavigate } from "react-router-dom";
+
 function Welcome({theme, validateTheme}){
+
+    const navigate = useNavigate();
+
+    function renderSignIn(){
+        setTimeout(() => {
+            navigate('/sign-in')
+        }, 5000)
+    }
+
     return (
-        <h1 className={`text-[48px] mt-[80px] font-bold ${validateTheme(theme, "text-slate-950", "text-slate-300")}`}>Seja Bem vindo!</h1>
+        <div>
+            <h1 className={`lg:text-[48px] md:text-[32px] text-[28px] mt-[80px] font-bold ${validateTheme(theme, "text-slate-950", "text-slate-300")}`}>Seja Bem vindo!</h1>
+            {renderSignIn()}
+        </div>
+        
+        
     )
 }
 
