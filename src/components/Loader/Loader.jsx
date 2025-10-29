51f65467ebdbd85a78f60864b05ff8ba86380aea
Thiago_de_Almeida_Brum <thiagobrum222@gmail.com>
2025-10-28 23:46:25 -0300

    Quinquagesimo quinto commit

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,7 +1,4 @@
-function Loader({ 
-    theme, 
-    validateTheme
-}){
+function Loader({ theme, validateTheme }){
     return (
         <div
             id="login-screen"
@@ -13,7 +10,7 @@ function Loader({
                 flex-col 
                 overflow-x-hidden
                 overflow-y-hidden
-                ${validateTheme(theme, "bg-slate-50", "bg-slate-900")}
+                ${validateTheme(theme,"bg-slate-50","bg-slate-900")}
         `}>
             <div className="h-[60px] flex items-center gap-1 absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] z-[2]">
                 <div className="h-[20px] w-[14px] rounded-full animate-loader "></div>
